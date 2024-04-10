46a48159764acbc6849934e4757aadab18eee6b4
Akmal <akmalthemalong@gmail.com>
2024-04-10 17:28:44 +0800

    add script for screen recording and screenshot

--- scripts/.config/scripts/screenshot.sh
+++ scripts/.config/scripts/screenshot.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-filedir=$(xdg-user-dir PICTURES)
+filedir="$(xdg-user-dir PICTURES)/Screenshots"
 filename="Screenshot_$(date +%Y%m%d)_$(date +%H%M%S).png"
 filepath="$filedir/$filename"
+echo $filepath
 
 if [[ $1 == "full" ]]; then
   grim -l 1 $filepath
