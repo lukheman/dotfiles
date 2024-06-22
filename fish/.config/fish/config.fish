2d2f01fe81c9d2488717c73c1ded81b1fd78561b
Akmal <akmalthemalong@gmail.com>
2024-06-22 16:40:29 +0800

    add fzf and fd ability

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,38 @@
-if status is-interactive
-and not set -q TMUX
-    exec tmux
+# if status is-interactive
+# and not set -q TMUX
+#     exec tmux
+# end
+
+function fzf_tmux
+    set selected_dir (fd --type d --hidden --follow --exclude .git . $HOME | fzf --height 40% --reverse --border)
+    if test -n "$selected_dir"
+        set session_name (basename "$selected_dir")
+
+        # Check if running inside tmux
+        if test -n "$TMUX"
+          cd "$selected_dir"
+          # if tmux list-windows -F "#{window_name}" | grep -q "^$window_name\$"
+          #   tmux select-window -t "$window_name"
+          # else
+          #     # tmux new-window -c "$selected_dir" -n "$window_name"
+          #   tmux new-window -c "$selected_dir"
+          # end
+        else
+          tmux new-session -s "$session_name" -c "$selected_dir"
+        end
+    end
 end
 
+function fzf_open_with_editor
+    set selected_file (fd --type f --hidden --follow --exclude .git . | fzf --height 40% --reverse --border)
+    if test -n "$selected_file"
+        $EDITOR "$selected_file"
+    end
+end
+
+# Binding Ctrl+F to the fzf_tmux function
+alias f=fzf_tmux
+alias ff=fzf_open_with_editor
 
 export EDITOR="nvim"
 
