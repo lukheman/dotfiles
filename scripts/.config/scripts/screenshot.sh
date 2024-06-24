e7dc1a51849710451d9d1c0eece48a2a2aff2ead
Akmal <akmalthemalong@gmail.com>
2024-06-24 08:27:43 +0800

    screenshot script for x 11

--- scripts/.config/scripts/screenshot.sh
+++ scripts/.config/scripts/screenshot.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
-filedir="$(xdg-user-dir PICTURES)/Screenshots"
+# filedir="$(xdg-user-dir PICTURES)/Screenshots"
+filedir="$HOME/Pictures/Screenshots"
 filename="Screenshot_$(date +%Y%m%d)_$(date +%H%M%S).png"
 filepath="$filedir/$filename"
-echo $filepath
+
+
+# if [[ $1 == "full" ]]; then
+#   grim -l 1 $filepath
+#   swappy -f $filepath
+#   wl-copy $filepath
+# elif [[ $1 == "select" ]]; then
+#   grim -l 1 -g "$(slurp)" $filepath
+#   swappy -f $filepath
+#   wl-copy $filepath
+# fi
 
 if [[ $1 == "full" ]]; then
-  grim -l 1 $filepath
-  swappy -f $filepath
-  wl-copy $filepath
+  scrot $filepath
+  xclip -selection clipboard -t image/png -i $filepath
 elif [[ $1 == "select" ]]; then
-  grim -l 1 -g "$(slurp)" $filepath
-  swappy -f $filepath
-  wl-copy $filepath
+  # grim -l 1 -g "$(slurp)" $filepath
+  scrot $filepath
 fi
 
 notify-send "Screenshot: $filepath"
