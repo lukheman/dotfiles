9f3ab027f164565dd9c19d9736dc234f5b385bb3
Akmal <akmalthemalong@gmail.com>
2024-07-18 12:38:56 +0800

    add: folder exclude

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,21 +4,23 @@
 # end
 
 function fzf_tmux
-    set selected_dir (fd --type d --hidden --follow --exclude .git --exclude env . $HOME | fzf --height 40% --reverse --border)
+    set selected_dir (fd --type d --hidden --follow --exclude .local --exclude .git --exclude .cache --exclude env . $HOME | fzf --reverse)
     if test -n "$selected_dir"
         set session_name (basename "$selected_dir")
 
         # Check if running inside tmux
         if test -n "$TMUX"
-          tmux new-window -c "$selected_dir" -n "$window_name"
+          # tmux new-window -c "$selected_dir" -n "$window_name"
+          cd "$selected_dir"
         else
+          tmux kill-server
           tmux new-session -s "$session_name" -c "$selected_dir"
         end
     end
 end
 
 function fzf_open_with_editor
-    set selected_file (fd --type f --hidden --follow --exclude .git . | fzf --height 40% --reverse --border)
+    set selected_file (fd --type f --hidden --follow --exclude .git --exclude .local --exclude .cache --exclude env --exclude .ssh . | fzf --reverse --preview="bat --color=always {}")
     if test -n "$selected_file"
         $EDITOR "$selected_file"
     end
@@ -36,6 +38,7 @@ alias ff=fzf_open_with_editor
 alias ls="exa --icons --sort type"
 alias lsa="ls -a"
 alias cat="bat"
+alias ncm="ncmpcpp"
 
 # config
 alias viconf="cd ~/.config/nvim"
@@ -43,6 +46,7 @@ alias viconf="cd ~/.config/nvim"
 # text editor
 alias vi="nvim"
 alias hx="helix"
+alias vidb="nvim -c \":Dbee\""
 
 # git
 alias gst="git status"
