1a4ba808a779fe547411da44529882111a9a3679
Akmal <akmalthemalong@gmail.com>
2024-08-17 18:03:31 +0800

    add palette

---
+++ palette/onedark.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+background="#282c34"
+foreground="#abb2bf"
+black="#181a1f"
+white="#fffefe"
+red="#e06c75"
+green="#98c379"
+blue="#61afef"
+orange="#d19a66"
+yellow="#e5c07b"
+purple="#c678dd"
+cyan="#56b6c2"
+grey="#5c6370"
