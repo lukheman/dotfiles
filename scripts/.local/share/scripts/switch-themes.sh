91fcc918401033eca2fa8b6cc68b2469457615e5
Akmal <akmalthemalong@gmail.com>
2024-08-18 19:43:44 +0800

    add themes to switch theme script

--- scripts/.local/share/scripts/switch-themes.sh
+++ scripts/.local/share/scripts/switch-themes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 
-themes=$(echo -e "onedark\nsolarized" | rofi -dmenu)
+themes=$(echo -e "onedark\nsolarized" | rofi -dmenu -theme ~/.config/rofi/main.rasi)
 
 bash "$HOME/dotfiles/themes/$themes/apply.sh"
