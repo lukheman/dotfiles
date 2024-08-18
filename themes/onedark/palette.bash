fe471a19b058788ea1167319610dd11ed93c864c
Akmal <akmalthemalong@gmail.com>
2024-08-18 17:37:57 +0800

    create themes: onedark and solarized

---
+++ themes/onedark/palette.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+background="#282c34"
+foreground="#abb2bf"
+black="#21252b"
+white="#fffefe"
+red="#e06c75"
+green="#98c379"
+blue="#61afef"
+yellow="#d19a66"
+purple="#c678dd" # also known as magenta
+cyan="#56b6c2"
+
+
+# not use
+# orange="#d19a66"
+# grey="#5c6370"
+# yellow="#e5c07b"
