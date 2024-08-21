490225f54633f37c069cc91638f8c3b2460bcb2e
Akmal <akmalthemalong@gmail.com>
2024-08-21 19:06:48 +0800

    disable dracula theme

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,11 +1,12 @@
-import dracula.draw
+# import dracula.draw
+# import onedark.draw
 
 # Load existing settings made via :set
 config.load_autoconfig()
 
-dracula.draw.blood(c, {
-    'spacing': {
-        'vertical': 6,
-        'horizontal': 8
-    }
-})
+# onedark.draw.blood(c, {
+#     'spacing': {
+#         'vertical': 2,
+#         'horizontal': 2
+#     }
+# })
