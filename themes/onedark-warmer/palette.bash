1dbd081dddc63c160584c9ee82c86044866519a9
Akmal <akmalthemalong@gmail.com>
2025-01-09 07:08:54 +0800

    create: onedark-warmer theme

---
+++ themes/onedark-warmer/palette.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+background="#232326"
+foreground="#a7aab0"
+black="#1b1c1e"
+white="#d6d7d9"
+red="#de5d68"
+green="#8fb573"
+blue="#57a5e5"
+yellow="#dbb671" # orange = "#c49060",
+purple="#bb70d2"
+cyan="#51a8b3"
