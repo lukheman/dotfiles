274dded60bf66e3e8dbd4be101179e669b6ae29c
Akmal <akmalthemalong@gmail.com>
2025-01-09 07:08:31 +0800

    update: screenshot script

--- scripts/.local/share/scripts/screenshot.sh
+++ scripts/.local/share/scripts/screenshot.sh
@@ -5,23 +5,25 @@ filedir="$HOME/Pictures/Screenshots"
 filename="Screenshot_$(date +%Y%m%d)_$(date +%H%M%S).png"
 filepath="$filedir/$filename"
 
+# using imagemagic and xdotool
+import -window "$(xdotool getactivewindow)" $filepath
+
+ # if [[ $1 == "full" ]]; then
+ #   grim -l 1 $filepath
+ #   swappy -f $filepath
+ #   wl-copy $filepath
+ # elif [[ $1 == "select" ]]; then
+ #   grim -l 1 -g "$(slurp)" $filepath
+ #   swappy -f $filepath
+ #   wl-copy $filepath
+ # fi
 
 # if [[ $1 == "full" ]]; then
-#   grim -l 1 $filepath
-#   swappy -f $filepath
-#   wl-copy $filepath
+#   scrot $filepath
+#   xclip -selection clipboard -t image/png -i $filepath
 # elif [[ $1 == "select" ]]; then
-#   grim -l 1 -g "$(slurp)" $filepath
-#   swappy -f $filepath
-#   wl-copy $filepath
+#   # grim -l 1 -g "$(slurp)" $filepath
+#   scrot $filepath
 # fi
-
-if [[ $1 == "full" ]]; then
-  scrot $filepath
-  xclip -selection clipboard -t image/png -i $filepath
-elif [[ $1 == "select" ]]; then
-  # grim -l 1 -g "$(slurp)" $filepath
-  scrot $filepath
-fi
 
 notify-send "Screenshot: $filepath"
